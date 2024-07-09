4758cda7fbe79aaa497386dc30ef47265472de98
Amirhossein Ghanipour <d3v1ll3n@gmail.com>
2024-07-09 08:07:12 +0330

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 DESCRIPTION = 'JAXTRA'
 LONG_DESCRIPTION = 'A package for building and training neural networks with JAX.'
 
