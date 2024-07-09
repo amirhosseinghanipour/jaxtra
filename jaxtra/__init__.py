3d1f6b075148ca2bc118f84d857f497a55a4a480
Amirhossein Ghanipour <d3v1ll3n@gmail.com>
2024-07-09 03:53:33 +0330

    Define the public package API

---
+++ jaxtra/__init__.py
@@ -0,0 +1,15 @@
+from .layers import InputLayer, Dense, Conv1D, Conv2D, Conv3D, MaxPooling1D, MaxPooling2D, MaxPooling3D, Dropout, Flatten, BatchNormalization
+
+__all__ = [
+    'InputLayer',
+    'Dense',
+    'Conv1D',
+    'Conv2D',
+    'Conv3D',
+    'MaxPooling1D',
+    'MaxPooling2D',
+    'MaxPooling3D',
+    'Dropout',
+    'Flatten',
+    'BatchNormalization',
+]
